89ae51669fbb218271edd3f614ececacbeefbd01
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2026-01-02 12:11:46 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className='text-4xl font-bold text-center text-white underline'>React-Mini-Projects-Level-Beginner</h1>
+    </>
+  )
+}
+
+export default App
