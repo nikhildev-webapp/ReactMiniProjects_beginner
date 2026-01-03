e49367a5f8a85e157f91fc4ca7f745e085f714ac
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2026-01-03 19:45:43 +0530

    reset the css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
-import ColorThemeSwitcher from './components/ColorThemeSwitcher'
+
 
 function App() {
    return (
     <>
       <h1 className='text-4xl font-bold text-center underline'>React-Mini-Projects-Level-Beginner</h1>
-     <ColorThemeSwitcher/>
+    
     </>
   )
 }
