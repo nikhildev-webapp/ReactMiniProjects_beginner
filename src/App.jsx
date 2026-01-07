1762bec2f4a6cb7b9a402e50e3ba43e106b410ba
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2026-01-07 18:17:46 +0530

    clean the app.jsx file to create next mini project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
-import ProductCard from './components/ProductCard/ProductCard'
-import ProductList from './components/ProductCard/ProductList'
 
 function App() {
    return (
     <>
       <h1 className='text-4xl font-bold text-center underline'>React-Mini-Projects-Level-Beginner</h1>
-      <ProductList/>
+     
     </>
   )
 }
